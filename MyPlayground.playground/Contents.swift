b42af176de1f3dc54de8560f761fa0aa85381d68
jmvarguez-web <jmvarguez@hotmail.com>
2015-06-06 10:13:39 -0500

    Variables y constantes

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,108 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+var miNumero = 4
+var otroNumero = 0.890
+var misuma = 10
+
+var miNombre = "Jesus"
+var miApellido = "Varguez"
+
+var miNombreCompleto = ""
+
+miNombreCompleto = miNombre + " " + miApellido;
+
+miNombreCompleto = miNombre + "\(miApellido)";
+
+let miConstante = 10
+let miApodo = "Elcompa"
+var miEntero: Int;
+miEntero=1200
+
+let miConstante2: Int = 4
+
+var miFlotante:Float = 1.23
+
+var miDouble:Double = 1233.123333
+
+var mibinario=0b0000
+var miheradeximal = 0x001
+var miOctal=0o1
+
+var miNumerogigante=590e20
+
+var x = 34.5
+var y = 56.9
+
+var suma = (x + y)
+var numeroExtraLarge = 458_912_901_209_129_012
+
+let JesusinvitalasDonas = true
+//Esto es una tupla
+let alumnoUNID = (1,"Jesus Manuel")
+let (miID,miAlumno) = alumnoUNID
+
+var dimeID = miID
+var dimeAlumno = miAlumno
+
+//ARREGLOS
+
+var miArregloXtremeGym = ["Andres","Hector","Jack"]
+var misReprobados = [0,5,5,6]
+
+var misDeuda = [-1234.78,233.6,345.900]
+
+//var minuevoArray:String[] = []
+
+var miArregloLimpio = [String]()
+
+
+var miArrayEnteros=[Int]()
+//AGREGAR un Nuevo elemento
+miArrayEnteros.append(3)
+//LIMPIAR ARREGLO
+miArrayEnteros=[]
+miArrayEnteros.append(3)
+miArrayEnteros.append(3)
+miArrayEnteros.append(2)
+miArrayEnteros.append(3)
+miArrayEnteros.append(3)
+
+miArrayEnteros.insert(1, atIndex: 2)
+miArrayEnteros.insert(0, atIndex: 0)
+miArrayEnteros.count
+miArrayEnteros.insert(1000, atIndex: miArrayEnteros.count)
+miArrayEnteros.removeLast()
+
+miArrayEnteros.removeLast()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
